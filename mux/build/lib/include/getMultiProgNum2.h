eeba0d405703c086e16b605188fed74e17228b71
Stan Hangzhou <stanhangzhou@gmail.com>
2014-12-25 12:22:57 +0800

    del build folder

--- mux/build/lib/include/getMultiProgNum2.h
+++
@@ -1,8 +0,0 @@
-#ifndef _GET_MULTIPROGNUM2_H_
-#define _GET_MULTIPROGNUM2_H_
-
-#include "datastructdef.h"
-
-int getMultiProgNum2(char *ip, out_program_num_t * outprg);
-
-#endif 
--- mux/build/lib/include/getOutPidMapNum.h
+++
@@ -1,6 +0,0 @@
-#ifndef  _GET_OUTPIDMAPNUM_
-#define _GET_OUTPIDMAPNUM_
-
-int getOutPidMapNum(char *ip, unsigned short *result)
-
-#endif
