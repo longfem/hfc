f1c74b486ae7c282f39ba91821af212692fd3881
Stan Hangzhou <stanhangzhou@gmail.com>
2015-01-05 09:21:03 +0800

    clear unuseful file

--- muxnms/controllers/build/lib/include/getOutRate.h
+++
@@ -1,6 +0,0 @@
-#ifndef _GET_OUTRATE_H_
-#define _GET_OUTRATE_H_
-
-int getOutRate(char *ip, int* result);
-
-#endif
--- muxnms/controllers/build/lib/include/getPrograms.h
+++
@@ -1,9 +0,0 @@
-#ifndef _GET_PROGRAMS_H_
-#define _GET_PROGRAMS_H_
-
-#include "datastructdef.h"
-
-
-int getPrograms(char *ip, int inChn, char *outprg);
-
-#endif 
