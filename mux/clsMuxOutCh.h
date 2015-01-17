365b4dc4f4be7f83ef0e888f57393fbb2a8536bd
Stan Hangzhou <stanhangzhou@gmail.com>
2015-01-17 13:45:21 +0800

    addget valid rate and out channal status

--- mux/clsMuxOutCh.h
+++ mux/clsMuxOutCh.h
@@ -5,7 +5,8 @@
 #ifndef _CLS_MUX_OUT_CHANNEL_H_
 #define _CLS_MUX_OUT_CHANNEL_H_
 
-
+ErrorTypeEm OutChn_validBitrateGet(char *ip, int outChnId, unsigned int *outValidBitrate);
+ErrorTypeEm GetOutChannelStatus(char *ip, int outChn, unsigned int *outStatus);
 ErrorTypeEm GetOutChnTSID(char *ip, int outChn,  unsigned int  *outTsId);
 ErrorTypeEm SetOutChnTSID(char *ip, int outChn,  unsigned int  outTsId);
 ErrorTypeEm GetOutChnNetID(char *ip, int outChn, unsigned int  *outNetId);
