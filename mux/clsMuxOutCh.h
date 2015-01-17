2e87ffdf450520cf795d122967c48a884b1d7a8f
Stan Hangzhou <stanhangzhou@gmail.com>
2015-01-17 13:34:36 +0800

    add header file

--- mux/clsMuxOutCh.h
+++ mux/clsMuxOutCh.h
@@ -7,10 +7,15 @@
 
 
 ErrorTypeEm GetOutChnTSID(char *ip, int outChn,  unsigned int  *outTsId);
+ErrorTypeEm SetOutChnTSID(char *ip, int outChn,  unsigned int  outTsId);
 ErrorTypeEm GetOutChnNetID(char *ip, int outChn, unsigned int  *outNetId);
+ErrorTypeEm SetOutChnNetID(char *ip, int outChn, unsigned int  outNetId);
 ErrorTypeEm GetOutChnOrgNetID(char *ip, int outChn, unsigned int *outOrgNetId);
+ErrorTypeEm SetOutChnOrgNetID(char *ip, int outChn, unsigned int outOrgNetId);
 ErrorTypeEm GetOutChnTableVer(char *ip, int outChn, unsigned int *outTableVer);
+ErrorTypeEm SetOutChnTableVer(char *ip, int outChn, unsigned int outTableVer);
 ErrorTypeEm GetTableEnableFlag(char *ip, int outChn, unsigned int *outStatus);
+ErrorTypeEm SetTableEnableFlag(char *ip, int outChn, unsigned int outStatus);
 #endif
 
 
