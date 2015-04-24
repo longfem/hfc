b81ed17889d91265dfc6abaea1117e8242959e80
Stan Hangzhou <stanhangzhou@gmail.com>
2015-04-24 10:52:00 +0800

    version 0.05

--- muxnms/controllers/programs.c
+++ muxnms/controllers/programs.c
@@ -27,8 +27,8 @@ extern ClsProgram_st clsProgram;
 extern ClsParams_st *pdb;
 
 
-char ip[16] = "192.168.1.49";
-//char ip[16] = "127.0.0.1";
+//char ip[16] = "192.168.1.49";
+char ip[16] = "127.0.0.1";
 char optstr[256] = {0};
 
 static void rendersts(char *str,int status)
